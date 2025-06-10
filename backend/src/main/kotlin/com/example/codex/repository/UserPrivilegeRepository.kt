9141f9269f3036bbf42d02f5cbbb249a09937dab
Bálint Györffy <gyorffy.balint@hellopay.hu>
2025-06-10 03:45:36 +0200

    fix

--- backend/src/main/kotlin/com/example/codex/repository/UserPrivilegeRepository.kt
+++ backend/src/main/kotlin/com/example/codex/repository/UserPrivilegeRepository.kt
@@ -1,13 +1,15 @@
 package com.example.codex.repository
 
 import com.example.codex.domain.UserPrivilege
-import com.example.codex.jooq.tables.UserPrivilege as UserPrivilegeTable
 import com.example.codex.jooq.tables.references.USER_PRIVILEGE
 import org.jooq.DSLContext
 import org.springframework.stereotype.Repository
+import com.example.codex.jooq.tables.UserPrivilege as UserPrivilegeTable
 
 @Repository
 class UserPrivilegeRepository(
     override val dslContext: DSLContext
 ) : CrudRepository<UserPrivilegeTable, UserPrivilege> {
+    override val table = USER_PRIVILEGE
+    override val type = UserPrivilege::class.java
 }
