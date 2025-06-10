1ed7b8066a61be7f0be9857c94b35ef8f68342c9
Györffy Bálint <52378106+balint1111@users.noreply.github.com>
2025-06-10 04:20:36 +0200

    Adjust language selector position and size

--- frontend/src/top-menu.component.ts
+++ frontend/src/top-menu.component.ts
@@ -8,17 +8,18 @@ import { TranslationService, Lang } from './i18n/translation.service';
     <mat-toolbar color="primary">
       <span *ngIf="user" class="user">{{ 'WELCOME' | t:{username: user?.username} }}</span>
       <mat-slide-toggle [(ngModel)]="dark" (change)="toggleDark()">{{ 'DARK_MODE' | t }}</mat-slide-toggle>
-      <mat-select [(ngModel)]="lang" (selectionChange)="changeLang($event.value)">
+      <span class="spacer"></span>
+      <mat-select class="lang" [(ngModel)]="lang" (selectionChange)="changeLang($event.value)">
         <mat-option value="en">EN</mat-option>
         <mat-option value="hu">HU</mat-option>
       </mat-select>
-      <span class="spacer"></span>
       <button mat-button *ngIf="user" (click)="logout.emit()">{{ 'LOGOUT' | t }}</button>
     </mat-toolbar>
   `,
   styles: [`
     .spacer { flex: 1 1 auto; }
     .user { margin-right: 1rem; }
+    .lang { width: 60px; margin-right: .5rem; }
   `]
 })
 export class TopMenuComponent {
